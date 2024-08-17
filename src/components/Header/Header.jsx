dda00cf71c8910cd824bd8ae2e740f1d59dfdd1c
rakshajay <rakshashettyhs@gmail.com>
2024-08-16 19:24:51 -0700

    header done for all screens

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import logo from "../../assets/Logo/BrainFlix-logo.svg";
+import murgan from "../../assets/Images/Mohan-muruge.jpg";
+import buttonIcon from "../../assets/Icons/upload.svg";
+import "./header.scss";
+
+function Header() {
+  return (
+    <header>
+      <nav className="header">
+        <div className="header-content__logo">
+          <img src={logo} alt="BrainFlix Logo" />
+        </div>
+        <div className="header-content__search">
+          <input
+            type="text"
+            placeholder="Search"
+            className="header-content__search-input"
+          />
+          <img className="header-content__search-murgan"src={murgan} alt="Murgan face image" />
+        </div>
+        <div className="header-content__button">
+          <button>
+              <h3>UPLOAD</h3>
+          </button>
+          <img className="header-content__button-murgan"src={murgan} alt="Murgan face image" />
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
