c173aa43a292de969a0576d1be90260612b04139
rakshajay <rakshashettyhs@gmail.com>
2024-08-18 15:05:11 -0700

    app.jsx updated with divs and styling added to suit desktop layout, comments scetion button width fixed, othet minor corrections to match exaactly to mockup guide

--- src/App.jsx
+++ src/App.jsx
@@ -9,40 +9,42 @@ import NextVideos from "./components/NextVideos/NextVideos";
 import dataArray from "./data/video-details.json";
 
 function App() {
-  
   const openingVideo = dataArray.find(
     (video) => video.id === "84e96018-4022-434e-80bf-000ce4cd12b8"
   );
 
-
   const [currentVideo, setCurrentVideo] = useState(openingVideo);
-  
+
   const handleVideoClick = (id) => {
     const foundVideo = dataArray.find((video) => video.id === id);
     setCurrentVideo(foundVideo);
   };
 
-
   return (
     <>
       <Header />
       <MainVideo poster={currentVideo.image} video={currentVideo.video} />
-      <DescriptionSection
-      description={currentVideo.description}
-      title={currentVideo.title}
-      channel={currentVideo.channel}
-      views={currentVideo.views}
-      likes={currentVideo.likes}
-      timestamp={new Date(currentVideo.timestamp).toLocaleDateString()}
-      />
-      <CommentSection commentsNumber={currentVideo.comments.length}/>
-      <CommentList 
-      dataArrayComments={currentVideo.comments} />
-      <NextVideos 
-      dataArray={dataArray} 
-      handleVideoClick={handleVideoClick}
-      currentVideoID={currentVideo.id}
-      />
+      <div className="info">
+        <div className="info-text">
+          <DescriptionSection
+            description={currentVideo.description}
+            title={currentVideo.title}
+            channel={currentVideo.channel}
+            views={currentVideo.views}
+            likes={currentVideo.likes}
+            timestamp={new Date(currentVideo.timestamp).toLocaleDateString()}
+          />
+          <CommentSection commentsNumber={currentVideo.comments.length} />
+          <CommentList dataArrayComments={currentVideo.comments} />
+        </div>
+       <div className="info-list">
+       <NextVideos
+          dataArray={dataArray}
+          handleVideoClick={handleVideoClick}
+          currentVideoID={currentVideo.id}
+        />
+       </div>
+      </div>
     </>
   );
 }
